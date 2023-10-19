d68abb64516702e2ba7acb5b41c4f4047c956581
Bradley <61959010+Wempt@users.noreply.github.com>
2023-10-19 12:26:56 -0700

    Changed File not found handeling and added creation of output file

--- 06/assembler0/Assembler.java
+++ 06/assembler0/Assembler.java
@@ -1,23 +1,35 @@
 package assembler0;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Assembler {
     public static void main(String[] args) {
         if (args.length == 0) { // No file
-            System.println("No source file");
+            System.out.println("No source file");
             System.exit(1);
         }
-        try { // File not found
-            File infile = new File(args[0].trim());
-        }
-        catch(FileNotFoundException e) {
-            e.printStackTrace();
+        File infile = new File(args[0].trim());
+        if (!infile.exists()) {
             System.out.println("File not found");
+            System.exit(1);
+        }
+        String infilePath = infile.getAbsolutePath();
+        String outfilePath = infilePath.substring(0, infilePath.lastIndexOf("/")+1);
+        String infileName = infile.getName();
+        String infileNoExt = infileName.substring(0,infileName.lastIndexOf("."));
+        File outfile = new File(outfilePath + infileNoExt + ".hack");
+
+        if (outfile.exists()) {
+            outfile.delete();
+        }
+        try {
+            outfile.createNewFile();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
         }
 
-        
+
     }
 }
