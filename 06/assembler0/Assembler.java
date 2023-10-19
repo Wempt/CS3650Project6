6c16abacfb6c33c63cb7bc698436c2aa68327786
Bradley <61959010+Wempt@users.noreply.github.com>
2023-10-19 15:35:17 -0700

    Added parse method that translates the input file and writes to output file

--- 06/assembler0/Assembler.java
+++ 06/assembler0/Assembler.java
@@ -2,6 +2,8 @@ package assembler0;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class Assembler {
     public static void main(String[] args) {
@@ -10,6 +12,7 @@ public class Assembler {
             System.exit(1);
         }
         File infile = new File(args[0].trim());
+        // File infile = new File("rect/RectL.asm");
         if (!infile.exists()) {
             System.out.println("File not found");
             System.exit(1);
@@ -25,11 +28,37 @@ public class Assembler {
         }
         try {
             outfile.createNewFile();
+            parse(infile, outfile);
         }
         catch(IOException e) {
             e.printStackTrace();
         }
-
-
+    }
+    private static void parse(File infile, File outfile) throws IOException{
+        Parser parser = new Parser(infile);
+        Code decode = new Code();
+        FileWriter fWriter = new FileWriter(outfile);
+        BufferedWriter writer = new BufferedWriter(fWriter);
+        
+        while(parser.hasMoreCommands()) {
+            parser.advance();
+            CommandType cType = parser.commandType();
+            String instruction = null;
+            if (cType.equals(CommandType.A_COMMAND)) {
+                String symbol = parser.symbol();
+                String binary = Integer.toBinaryString(Integer.parseInt(symbol));
+                instruction = String.format("%16s",binary).replace(" ", "0");
+            }
+            else if (cType.equals(CommandType.C_COMMAND)) {
+                instruction = "111" + decode.comp(parser.comp()) + decode.dest(parser.dest()) + decode.jump(parser.jump());
+            }
+            if (!cType.equals(CommandType.L_COMMAND)) {
+                //write to file
+                System.out.println(instruction);
+                writer.append(instruction);
+                writer.newLine();
+            }
+        }
+        writer.close();
     }
 }
